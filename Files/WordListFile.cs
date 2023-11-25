36b686e8528687aaaae108c892a1a0ec47de733a
Ian Bailey-Mortimer <ian@b-m.id.au>
2023-11-25 16:47:30 +1000

    loads words and clues

--- Files/WordListFile.cs
+++ Files/WordListFile.cs
@@ -1,5 +1,44 @@
+using CrosswordMaker.Words;
+
 namespace CrosswordMaker.Files;
 
-class WordListFile {
+static class WordListFile
+{
+    static string ToUppercaseLetters(string word)
+    {
+        StringBuilder sb = new();
+        foreach (char ch in word)
+        {
+            if (ch >= 'A' && ch <= 'Z')
+                sb.Append(ch);
+            else if (ch >= 'a' && ch <= 'z')
+                sb.Append(char.ToUpper(ch));
+        }
+        return sb.ToString();
+    }
+
+    public static async Task<List<DefinedWord>> LoadWordsAsync(string path)
+    {
+        List<DefinedWord> words = new();
+
+        using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous | FileOptions.SequentialScan);
+        using var reader = new StreamReader(stream);
+        
+        string? line;
+        while ((line = await reader.ReadLineAsync()) != null)
+        {
+            int cx = line.IndexOf('=');
+            if (cx < 2)
+                continue;
+
+            string wd = ToUppercaseLetters(line[..cx]);
+            if (wd.Length < 2)
+                continue;
+
+            words.Add(new(wd, line[(cx + 1)..]));
+        }
+
+        return words;
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,32 @@
-﻿namespace CrosswordMaker;
+﻿// using Mono.Options;
+
+using CrosswordMaker.Files;
+using CrosswordMaker.Words;
+
+namespace CrosswordMaker;
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // var opts = new OptionSet() {
+        //     { "" }
+        // };
+
+        Dictionary<string, DefinedWord> allWords = new();
+
+        foreach (var path in args) {
+            var fileWords = await WordListFile.LoadWordsAsync(path);
+            Console.WriteLine($"{path}: {fileWords.Count} words");
+
+            int skipped = 0;
+            foreach (var word in fileWords) {
+                if (!allWords.ContainsKey(word.Word))
+                    allWords[word.Word] = word;
+                else
+                    ++skipped;
+            }
+            if (skipped > 0)
+                Console.WriteLine($"skipped {skipped} previously loaded");
+        }
     }
 }
